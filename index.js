6aa2041df21bb257e84fb8b7106673664b59769b
oumamoringa <richard.ouma@student.moringaschool>
2022-08-23 15:13:21 +0300

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+document.getElementById("dodger").style.background = "#FF69B4";
+
+
+const moveDodgerLeft = () => {
+  let distance = (dodger.style.left.replace("px", ""));
+  if (distance > 0) {
+    dodger.style.left = distance - 1 + "px";
+  }
+}
+const moveDodgerRight = () =>{
+  let distance = parseInt(dodger.style.left.replace("px", ""));
+  if (distance < 360) {
+    dodger.style.left = distance + 1 + "px";
+  }
+}
+
+dodger.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
